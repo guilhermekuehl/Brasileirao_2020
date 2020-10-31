084554080c74e4152f5d797259f0761be73f86a5
Entra21 <entra21@proway.treina>
2020-10-30 21:08:29 -0300

    controller products

---
+++ WebAPI/Controllers/GameResponse.cs
@@ -0,0 +1,10 @@
+namespace WebAPI.Controllers
+{
+    public class GameResponse
+    {
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public double Price { get; set; }
+
+    }
+}
